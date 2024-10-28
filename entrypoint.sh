7490f8067c41eae86f35ff2642111e7b721f9a27
Moritz Bellach <moritz.bellach@intranda.com>
2024-10-28 18:51:44 +0100

    fixing bashscript

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,41 +11,49 @@ set -f
 # not supported by dash. unsure about /bin/sh
 set -o pipefail
 
-# just giving it a nicer name
-# you can give apache extra args by just appending them to the docker command like this:
-# docker run -it rproxy-ssl-apache -t -C "LoadModule info_module modules/mod_info.so" -D DUMP_CONFIG
-EXTRA_ARGS="$@"
+export APACHE_CONFDIR=${SERVER_ROOT}/conf
+export SV=""
+# SSL stuff
+export PRIMARY_PORT="$HTTP_PORT"
+export LISTEN_HTTPS="# SSL disabled"
+export REDIR_XOR_COMMON="goobi-common.conf"
 
+export MOD_REMOTEIP="# mod_remoteip disabled"
+export SITEMAP=""
 
-env
+echo ""
+echo "### ENVIRONMENT VARS AT SCRIPT START \\/\\/\\/"
+env|sort
+echo "### ENVIRONMENT VARS AT SCRIPT START /\\/\\/\\"
+echo ""
 
-# some conveniece functions
 render_template () {
     local SRC="$1"
     local DEST="$2"
-    local VARLIST="$3"
+    #local VARLIST="$3"
+
+    echo ""
+    echo "### ENVIRONMENT VARS AT FUNCTION START \\/\\/\\/"
+    env|sort
+    echo "### ENVIRONMENT VARS AT FUNCTION START /\\/\\/\\"
+    echo ""
 
     RENDERVARS=""
-    for MYVAR in $(echo "$VARLIST" | tr " " "\n")
+    # using $SV global variable here to work around broken
+    # quoting / function arguments in bash :/
+    for MYVAR in $(echo "$SV" | tr " " "\n")
     do
         RENDERVARS="${RENDERVARS} \$${MYVAR}"
+        VALUE=$(echo "\$${MYVAR}" | envsubst "\$${MYVAR}")
+        echo "${MYVAR}: $VALUE"
     done
-    echo "DBG MYVARS $VARLIST"
-    echo "DBG REVARS $RENDERVARS"
     echo "rendering template $SRC"
     echo "to $DEST"
+    echo "DBG SV: $SV"
     echo "with vars: $RENDERVARS"
     envsubst "$RENDERVARS" < "$SRC" > "$DEST"
 }
 
-render_simple () {
-    local DEST="$1"
-    local VARLIST="$2"
-    local SRC="${DEST}.template"
-    render_template "$SRC" "$DEST" "$VARLIST"
-}
-
-APACHE_CONFDIR=${SERVER_ROOT}/conf
 
 # -v tests if a var is set
 # new since bash 4.2, if this doesn't work maybe just do: $cmd || true
@@ -53,10 +61,6 @@ if [[ -v SOLR_INCLUDES ]]; then
     echo -e "$SOLR_INCLUDES" > ${APACHE_CONFDIR}/solr-restrictions.conf
 fi
 
-# SSL stuff
-PRIMARY_PORT="$HTTP_PORT"
-LISTEN_HTTPS="# SSL disabled"
-REDIR_XOR_COMMON="goobi-common.conf"
 if [ $ENABLE_SSL -eq 1 ]
 then
     echo "Enabling SSL"
@@ -71,7 +75,8 @@ then
     SV="${SV} HTTPS_PORT"
     SV="${SV} PRIMARY_PORT"
     SV="${SV} LISTEN_HTTPS"
-    render_simple ${APACHE_CONFDIR}/https_vhost.conf "$SV"
+    render_template ${APACHE_CONFDIR}/https_vhost.conf.template \
+                    ${APACHE_CONFDIR}/https_vhost.conf
     # get certifcate once / ensure they are current
     echo "Running certbot to get LetsEncrypt Certificates"
     certbot certonly --standalone --http-01-port "$HTTP_PORT" --email "$LE_EMAIL" --agree-tos --no-eff-email -d "$SERVERNAME"
@@ -84,16 +89,17 @@ SV="${SV} SERVERADMIN"
 SV="${SV} HTTP_PORT"
 SV="${SV} PRIMARY_PORT"
 SV="${SV} REDIR_XOR_COMMON"
-render_simple ${APACHE_CONFDIR}/http_vhost.conf "$SV"
+render_template ${APACHE_CONFDIR}/http_vhost.conf.template \
+                ${APACHE_CONFDIR}/http_vhost.conf
 
 # render common config
 SV=""
 SV="${SV} CUSTOM_CONFIG"
 SV="${SV} REDIRECT_INDEX_TO"
-render_simple ${APACHE_CONFDIR}/goobi-common.conf "$SV"
+render_template ${APACHE_CONFDIR}/goobi-common.conf.template \
+                ${APACHE_CONFDIR}/goobi-common.conf
 
 
-MOD_REMOTEIP="# mod_remoteip disabled"
 if [ $USE_MOD_REMOTEIP -eq 1 ]
 then
    echo "Enabling mod_remoteip"
@@ -120,7 +126,8 @@ SV="${SV} CUSTOM_CONFIG"
 #SV="${SV} CONNECTOR_HTTP_PORT"
 #SV="${SV} CONNECTOR_PATH"
 #SV="${SV} CONNECTOR_CONTAINER"
-render_simple ${APACHE_CONFDIR}/httpd.conf "$SV"
+render_template ${APACHE_CONFDIR}/httpd.conf.template \
+                ${APACHE_CONFDIR}/httpd.conf
 
 # render viewer config section
 if [ $ENABLE_VIEWER -eq 1 ]
@@ -134,7 +141,8 @@ then
     SV="${SV} SOLR_PATH"
     SV="${SV} SOLR_CONTAINER"
     echo "Enabling Viewer"
-    render_simple ${APACHE_CONFDIR}/viewer.conf "$SV"
+    render_template ${APACHE_CONFDIR}/viewer.conf.template \
+                    ${APACHE_CONFDIR}/viewer.conf
 fi
 
 # render workflow config section
@@ -149,12 +157,12 @@ then
     SV="${SV} ITM_AJP_PORT"
     SV="${SV} ITM_PATH"
     SV="${SV} ITM_CONTAINER"
-    render_simple ${APACHE_CONFDIR}/workflow.conf "$SV"
+    render_template ${APACHE_CONFDIR}/workflow.conf.template \
+                    ${APACHE_CONFDIR}/workflow.conf
 fi
 
 
 # render robots.txt, optionally with sitemap
-SITEMAP=""
 if [[ "$SITEMAP_LOCATION" != "" ]]
 then
     echo "Enabling Sitemap"
@@ -164,7 +172,8 @@ SV=""
 SV="${SV} VIEWER_PATH"
 SV="${SV} WORKFLOW_PATH"
 SV="${SV} SITEMAP"
-render_template ${APACHE_CONFDIR}/robots.txt.template /var/www/robots.txt "$SV"
+render_template ${APACHE_CONFDIR}/robots.txt.template \
+                /var/www/robots.txt
 
 
 #cat ${APACHE_CONFDIR}/httpd.conf
@@ -184,8 +193,7 @@ render_template ${APACHE_CONFDIR}/robots.txt.template /var/www/robots.txt "$SV"
 rm -f ${SERVER_ROOT}/logs/httpd.pid
 
 echo "Starting Apache2..."
-echo "\$0 = $0"
 echo "with extra args:"
-echo "$EXTRA_ARGS"
-cat ${APACHE_CONFDIR}/http_vhost.conf
-exec httpd -DFOREGROUND "$EXTRA_ARGS"
+echo "$@"
+#cat ${APACHE_CONFDIR}/http_vhost.conf
+exec /usr/local/apache2/bin/httpd -DFOREGROUND "$@"
